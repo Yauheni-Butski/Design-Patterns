bd77d42ee9c53b0c14a0588550e670b0e75ff8ea
ybutski <ybutski@learning.com>
2016-07-27 15:47:15 +0300

    Decorator: Implement Decorator pattern

---
+++ Design-Patterns/Decorator/Decorators/BorderWidthStyle.cs
@@ -0,0 +1,14 @@
+﻿using Decorator.Decorators.Base;
+using Decorator.Elements.Base;
+
+namespace Decorator.Decorators
+{
+    public class BorderWidthStyle : StyleDecorator
+    {
+        public BorderWidthStyle(HtmlElement htmlElement, int value) : base(htmlElement)
+        {
+            _key = "border-width";
+            _value = value + "px";
+        }
+    }
+}
---
+++ Design-Patterns/Decorator/Decorators/TextColorStyle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+using Decorator.Decorators.Base;
+using Decorator.Elements.Base;
+
+namespace Decorator.Decorators
+{
+    public enum TextColors
+    {
+        Red,
+        Green,
+        Blue,
+        Pink
+    }
+
+    public class TextColorStyle : StyleDecorator
+    {
+        private readonly Dictionary<string, string> _colorCodes = new Dictionary<string, string>()
+        {
+            { "Red", "#FF0000" },
+            { "Green", "#00FF00" },
+            { "Blue", "#0000FF" },
+            { "Pink", "#C48189" }
+
+        };
+
+        public TextColorStyle(HtmlElement htmlElement, TextColors value) : base(htmlElement)
+        {
+            _key = "color";
+            var color = value.ToString();
+            _value = _colorCodes[color];
+        }
+    }
+}
