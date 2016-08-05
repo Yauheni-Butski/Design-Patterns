022e4ecdff8ebf04325c8962c77d447dae5b19b5
Yauheni-Butski <Yauheni-Butski@users.noreply.github.com>
2016-08-05 15:24:23 +0300

    Singleton: Implement Singleton pattern

---
+++ Design-Patterns/Singleton/IdentifierMaker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    internal sealed class IdentifierMaker
+    {
+        private static readonly Lazy<IdentifierMaker> Instance = new Lazy<IdentifierMaker>(
+                                                                         () => new IdentifierMaker());
+
+        private int _lastIdentifier;
+
+        private IdentifierMaker()
+        {
+           _lastIdentifier = 0;
+        }
+
+        public static IdentifierMaker GetInstance()
+        {
+            return Instance.Value;
+        }
+
+        public int GetIdentifier()
+        {
+            return Interlocked.Increment(ref _lastIdentifier);
+        }
+    }
+}
---
+++ Design-Patterns/Singleton/Worker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Singleton
+{
+    public class Worker
+    {
+        private readonly string _workerName;
+
+        public Worker(string workerName)
+        {
+            _workerName = workerName;
+        }
+
+        public void DoWork()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                IdentifierMaker instance = IdentifierMaker.GetInstance();
+                Thread.Sleep(3000);
+                var identifier = instance.GetIdentifier();
+                var message = string.Format("{0} getting identifier #{1}", _workerName, identifier);
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
